20f6c08f4c7e4ce69fcad252e87b028d1479c00c
Rabia <logs.cloud6solutions@gmail.com>
2025-07-24 12:30:09 -0400

    Imlement the "Complete" and "Exit" buttons

--- gui.py
+++ gui.py
@@ -11,9 +11,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Zohaib's To-Do App.",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -29,6 +34,7 @@ while True:
             todos.append(new_todos)
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
 
         case "Edit":
             todo_to_edit = values["todos"][0]
@@ -41,9 +47,21 @@ while True:
             functions.write_todos(todos)
 
             window["todos"].update(values=todos)
+            window["todo"].update(value="")
 
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+
+        case "Exit":
+            break
         case "todos":
-
             window["todo"].update(value=values["todos"][0])
 
         case sg.WIN_CLOSED:
